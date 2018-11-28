9fb366cb57ffc768bfe9d68fb65879f58a70339b
Nono <nono@bxb.com>
2018-11-28 15:22:11 +0800

    update style

--- src/components/MonthCalendar.vue
+++ src/components/MonthCalendar.vue
@@ -1,4 +1,5 @@
 <template>
+<div class="c-wrapper">
   <div class="calendar">
     <div class="calendar__title">{{ month + ' 月' }}</div>
     <div
@@ -14,14 +15,21 @@
     <div
       v-for="(dayObj, key) in showDays"
       class="calendar__day"
-      :class="{
-        'calendar__day--nextmonth': dayObj.isNextMonth
-      }"
       :key="`day${key}`"
     >
-      {{ dayObj.value }}
+      <div
+        @click="toggleDay(dayObj)"
+        class="day"
+        :class="{
+          'calendar__day--nextmonth': dayObj.isNextMonth,
+          'calendar--active': dayObj.active
+        }"
+      >
+        {{ dayObj.value }}
+      </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -42,59 +50,120 @@ export default {
       default: () => []
     }
   },
-  computed: {
-    showDays: {
-      get () {
-        if (!this.year || !this.month) return []
-        const activeDay = dayjs()
-          .set('year', this.year)
-          .set('month', this.month - 1)
-        const firstDay = activeDay.startOf('month').day()
-        const lastDate = activeDay.endOf('month').date()
-        const weekRow = firstDay === 6 ? 6 : 5
-        const WEEK = 7
-        let day = 0
-        const fullCol = Array.from(Array(weekRow * WEEK).keys())
-          .map(i => {
-            let value = firstDay <= i
-              ? day++ % lastDate + 1
-              : ''
-            return {
-              value,
-              active: false,
-              isNextMonth: day > lastDate
-            }
-          })
-        return fullCol
-      },
-      set (val) {
-
-      }
+  data () {
+    return {
+      showDays: []
     }
   },
   methods: {
+    initCalendar () {
+      if (!this.year || !this.month) return []
+      const activeDay = dayjs()
+        .set('year', this.year)
+        .set('month', this.month - 1)
+      const firstDay = activeDay.startOf('month').day()
+      const lastDate = activeDay.endOf('month').date()
+      const weekRow = firstDay === 6 ? 6 : 5
+      const WEEK = 7
+      let day = 0
+      const fullCol = Array.from(Array(weekRow * WEEK).keys())
+        .map(i => {
+          let value = firstDay <= i
+            ? day++ % lastDate + 1
+            : ''
+          return {
+            value,
+            active: false,
+            isNextMonth: day > lastDate
+          }
+        })
+      this.showDays = fullCol
+    },
     showDayTitle (day) {
       const dayMapping = ['日', '一', '二', '三', '四', '五', '六']
       return dayMapping[day]
+    },
+    toggleDay (dayObj) {
+      if (dayObj.isNextMonth) return
+      dayObj.active = !dayObj.active
     }
+  },
+  watch: {
+    year (val) {
+      this.initCalendar()
+    }
+  },
+  created () {
+    this.initCalendar()
   }
 }
 
 </script>
 
 <style lang="stylus" scoped>
+
+.c-wrapper
+  padding: 10px
 .calendar
+  background-color #fff
+  min-height: 288px
   display flex
-  text-align center
   flex-wrap: wrap
-  color: #222
+  text-align center
+  color: #353C46
+  border-radius: 2px
+  min-width: 0
+  position: relative
+  text-decoration: none
+  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12)
+  justify-content flex-start
+  align-content flex-start
   .calendar__title
-    font-size 22px
+    font-weight bold
     flex 100%
+    display flex
+    align-items center
+    justify-content center
+    border-bottom 1px solid rgba(#C4C4C4, 0.3)
+    font-size 18px
+    height: 50px
+    margin-bottom 12px
   & .calendar__title--weekend
     color: red
   .calendar__day
     flex 14.28%
+    display flex
+    justify-content center
+    align-items center
+    font-size 16px
+    height: 31px
+  .day
+    font-size 14px
+    cursor pointer
+    user-select none
+    width: 22px
+    height 22px
+    color: #5DB3D4
+    display flex
+    justify-content center
+    align-items center
+    &.calendar--active
+      background-color: rgba(#FFBABA, .5)
+      border-radius 3px
+      color #BCBCBC
+      position relative
+    &.calendar--active:after
+      content ''
+      display block
+      height: 10px
+      width 10px
+      background-color #fff
+      position absolute
+      top -5px
+      right -5px
+      border-radius 50%
+      background-image url('../../public/baseline-remove_circle-24px.svg')
+      background-size 100% 100%
   & .calendar__day--nextmonth
    color: #eaeaea
 </style>
--- src/components/YearCalendar.vue
+++ src/components/YearCalendar.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="container">
     <div class="container__year">
-      <span><button @click="addYear(-1)">back</button></span>
-      <span>{{ year }}</span>
-      <span><button @click="addYear(1)">next</button></span>
+      <!-- <span><button @click="addYear(-1)">back</button></span> -->
+      <span
+        v-for="i in 5"
+        :key="i"
+        class="year__chooser"
+        @click="changeYear(i)"
+      >
+        {{ i + year - 3 }}
+      </span>
+      <!-- <span><button @click="addYear(1)">next</button></span> -->
     </div>
-    <month-calendar
-      class="container__month"
-      v-for="n in 12"
-      :key="`month-${n}`"
-      :year="year"
-      :month="n"
-    >
-    </month-calendar>
+    <div class="container__months">
+      <month-calendar
+        class="container__month"
+        v-for="n in 12"
+        :key="`month-${n}`"
+        :year="year"
+        :month="n"
+      >
+      </month-calendar>
+    </div>
   </div>
 </template>
 
@@ -32,6 +41,10 @@ export default {
   methods: {
     addYear (val) {
       this.year += val
+    },
+    changeYear (idx) {
+      this.year = idx + this.year - 3
+      console.log(this.year)
     }
   }
 }
@@ -39,15 +52,41 @@ export default {
 
 <style lang="stylus" scoped>
 .container
-  display flex
-  flex-wrap wrap
+  border-radius: 2px
+  min-width: 0
+  position: relative
+  text-decoration: none
+  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12)
+  background-color #F6F6F3
   .container__year
-    font-size 36px
+    height 65px
+    background-color #fff
+    font-size 24px
     flex 100%
     text-align center
     display flex
-    justify-content space-between
+    .year__chooser
+      height 100%
+      flex: 1
+      cursor pointer
+      display flex
+      align-items center
+      justify-content center
+      color rgba(black, 0.9)
+      &:hover
+        background-color rgba(#666, 0.1)
+      &:nth-child(4n-3)
+        color rgba(black, 0.3)
+      &:nth-child(2n)
+        color rgba(black, 0.6)
+      &:nth-child(3)
+        box-shadow inset 0px -3px #4792BD
+  .container__months
+    flex-wrap wrap
+    display flex
+    padding: 15px
   .container__month
-    padding 8px
+    padding: 8px
     flex 16.66%
+
 </style>
