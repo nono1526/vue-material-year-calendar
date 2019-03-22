b7d516c9f94d44713dffc681b8c3090ca3e4bc08
nono1526 <nono1102137211@gmail.com>
2019-03-22 09:00:25 +0800

    Bugfix - Add lost comma

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
       year: 2019,
       activeDates: [{ date: '2019-03-13', className: 'red' }, { date: '2019-03-14', className: 'blue' }],
       // activeDates: ['2019-03-12', '2019-03-16'],
-      defaultClassName: ''
+      defaultClassName: '',
       showYearSelector: true
     }
   },
