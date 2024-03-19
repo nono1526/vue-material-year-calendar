62e10f898fd3ea97a983983117970f1812e31e14
Domingo Albújar Llombart <dmingoalb@gmail.com>
2024-03-19 20:00:31 +0100

    Update Spanish App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       <option value="en">English</option>
       <option value="pt">Português</option>
       <option value="de">Deutsch</option>
+      <option value="es">Spanish</option>
       <option value="pl">Polski</option>
       <option value="ru">Русский</option>
     </select>
